7e2f8e46187078dea86778bb353be0288b9496da
edgar615 <edgar615@gmail.com>
2018-09-12 12:57:34 +0800

    校验的单元测试

--- spring-boot-util-validator/src/test/java/com/github/edgar615/util/spring/validate/ValidateServiceTest.java
+++ spring-boot-util-validator/src/test/java/com/github/edgar615/util/spring/validate/ValidateServiceTest.java
@@ -1,6 +1,9 @@
 package com.github.edgar615.util.spring.validate;
 
+import com.github.edgar615.util.validation.Rule;
 import com.github.edgar615.util.validation.ValidationException;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.Assert;
@@ -32,8 +35,58 @@ public class ValidateServiceTest {
   public void testValidateMap2() {
     Map<String, Object> map = new HashMap<>();
     map.put("barcode", "LH20401111111111111");
+    Multimap<String, Rule> rules = ArrayListMultimap.create();
+    rules.put("macAddress", Rule.required());
+    try {
+      validateService.validate("insert_device",rules, map);
+    } catch (ValidationException e) {
+      return;
+    }
+    Assert.fail();
+  }
+
+  @Test
+  public void testValidateMap3() {
+    Map<String, Object> map = new HashMap<>();
+    map.put("barcode", "LH20401111111111111");
     try {
       validateService.validate("insert_device", map);
+    } catch (ValidationException e) {
+      Assert.fail();
+    }
+  }
+
+  @Test
+  public void testValidateBean() {
+    Device device = new Device();
+    try {
+      validateService.validate("insert_device", device);
+    } catch (ValidationException e) {
+      return;
+    }
+    Assert.fail();
+  }
+
+  @Test
+  public void testValidateBean2() {
+    Device device = new Device();
+    device.setBarcode("LH20401111111111111");
+    Multimap<String, Rule> rules = ArrayListMultimap.create();
+    rules.put("macAddress", Rule.required());
+    try {
+      validateService.validate("insert_device",rules, device);
+    } catch (ValidationException e) {
+      return;
+    }
+    Assert.fail();
+  }
+
+  @Test
+  public void testValidateBean3() {
+    Device device = new Device();
+    device.setBarcode("LH20401111111111111");
+    try {
+      validateService.validate("insert_device", device);
     } catch (ValidationException e) {
       Assert.fail();
     }
