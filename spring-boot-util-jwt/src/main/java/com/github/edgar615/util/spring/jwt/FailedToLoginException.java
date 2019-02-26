f58fdf9ad9bd4abe7f372a3a1d09219033773102
edgar615 <edgar615@gmail.com>
2019-02-26 18:01:12 +0800

    '删除无用类'

--- spring-boot-util-jwt/src/main/java/com/github/edgar615/util/spring/jwt/FailedToLoginException.java
+++
@@ -1,8 +0,0 @@
-package com.github.edgar615.util.spring.jwt;
-
-public class FailedToLoginException extends Exception {
-
-    public FailedToLoginException(String username) {
-        super(username);
-    }
-}
